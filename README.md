ea2491f11a420db79ed28d78566e1e214d77719d
Jorge Malo <jorge.maloing@gmail.com>
2022-03-31 20:25:16 -0500

    Server initial structure

--- README.md
+++
@@ -1,7 +0,0 @@
-# DEMO_PB_33
-
-## SUBTITULO DEMO_PB_33
-
-Hola, soy un contenido del README.md
-
-### SOY UN SUBSUBTITULO
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middlewares
+
+// Routes
+app.get('/mensaje', (req, res) => {
+  res.send('Hola a todos desde Heroku!')
+});
+
+app.listen(PORT, () => {
+  console.log('Server is up and running on port: ', PORT);
+})
